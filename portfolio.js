eed42268b7cb3c479f161dfbffa4bd1a2ad24974
Tavishi-007 <145979984+Tavishi-007@users.noreply.github.com>
2023-12-14 18:45:36 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var button = document.querySelector("button");
+var h = document.querySelector("h1");
+button.addEventListener("click",function(){
+h.innerHTML="Text Change";
+button.innerHTML="badal gya";
+})
